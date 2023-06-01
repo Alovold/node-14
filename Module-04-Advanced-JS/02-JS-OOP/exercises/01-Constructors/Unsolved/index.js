f5d73e33b1b01acc90732abbb3b78acacdcb28ce
Austin Lovold <austin@lovolds.com>
2023-06-01 16:35:05 -0500

    Several exercises completed, updating for travel

--- Module-04-Advanced-JS/02-JS-OOP/exercises/01-Constructors/Unsolved/index.js
+++ Module-04-Advanced-JS/02-JS-OOP/exercises/01-Constructors/Unsolved/index.js
@@ -1,7 +1,14 @@
 // TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
-
-// TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
+function Developer (name, tech) {
+    this.name = name;
+    this.tech = tech;
+    // TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
+    this.introduction = () => {
+        console.log(`Hi, I'm ${name} and I work with ${tech}`)
+    }
+}
 
 // TODO: Create a new object using the 'Developer' constructor
-
+let codeBro = new Developer("Austin", "JavaScript")
 // TODO: Call the 'introduction()' method on the new object
+codeBro.introduction();
